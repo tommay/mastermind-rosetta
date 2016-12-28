fb8739712d949c897267e24f03fb850fb42bc806
Tom May <tom@tommay.net>
2016-12-28 12:22:55 -0800

    Revert "Use Scala's currying.  IMO this makes this particular code less readable."
    This reverts commit f3d39bdf9954c05daaee5e9549d7850cbe0d93cf.

--- mastermind.scala
+++ mastermind.scala
@@ -15,20 +15,20 @@ object Mastermind {
 
   def computeStats(startingGuess: List[Int]) : Stats = {
     allCodes.foldLeft(new Stats) {
-      foldCode(startingGuess)
+      (accum, code) => foldCode(startingGuess, accum, code)
     }
   }
 
-  def foldCode(startingGuess: List[Int])(stats: Stats, code: List[Int])
+  def foldCode(startingGuess: List[Int], stats: Stats, code: List[Int])
     : Stats =
   {
-    foldGuess(code, 1, allCodes)(stats, startingGuess)
+    foldGuess(code, 1, allCodes, stats, startingGuess)
   }
 
   def foldGuess(code: List[Int],
 		depth: Int,
-		possibilities: List[List[Int]])
-	       (stats: Stats,
+		possibilities: List[List[Int]],
+		stats: Stats,
 		guess: List[Int]) : Stats =
   {
     val score = computeScore(guess, code)
@@ -39,7 +39,8 @@ object Mastermind {
       val remainingPossibilities =
 	possibilities.filter{computeScore(guess, _) == score}
       remainingPossibilities.foldLeft(stats) {
-	  foldGuess(code, depth + 1, remainingPossibilities)
+	(accum, guess) =>
+	  foldGuess(code, depth + 1, remainingPossibilities, accum, guess)
       }
     }
   }
